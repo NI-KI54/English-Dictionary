659f7fde7bd95383a85396be9ad5965cbf5577a6
NI-KI54 <nickmasih575@gmail.com>
2023-03-01 18:19:50 +0530

    adeed try and catch method

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 const inputEl = document.getElementById("input");
+const infoTextEl = document.getElementById("info-text");
+const meaningContainerEl = document.getElementById("meaning-container");
+const titleEl = document.getElementById("title");
+const meaningEl = document.getElementById("meaning");
 
+async function fetchAPI(word){
+    try {
 
+    infoTextEl.style.display = "block";
+    meaningContainerEl.style.display = "none";
+    
+    infoTextEl.innerText = `Searching the meaning of "${word}"`
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    const result = await fetch(url).then((res) => res.json());
 
-function fetchAPI(word){
-    console.log(word);
+    infoTextEl.style.display = "none";
+    meaningContainerEl.style.display = "block";
+    titleEl.innerText = result[0].word
+    meaningEl.innerText = result[0]
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+
 }
 
 
